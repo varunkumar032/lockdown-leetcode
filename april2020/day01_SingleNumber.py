a4276cb11c0de7740a444f68f20f494717a7463f
Varun Kumar G <varunkumar032@gmail.com>
2020-05-02 11:14:05 +0530

    day 01

---
+++ april2020/day01_SingleNumber.py
@@ -0,0 +1,15 @@
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+# Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+# Input: [2,2,1]
+# Output: 1
+
+# Input: [4,1,2,1,2]
+# Output: 4
+
+from functools import reduce
+def singleNumber(nums):
+    return reduce(lambda x, y:x^y, nums)
+
+print(singleNumber([7,1,2,1,2]))
