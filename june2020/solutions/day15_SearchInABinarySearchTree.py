7dca0c040a6336059b6567144ed9b3edac573a56
Varun Kumar G <varunkumar032@gmail.com>
2020-06-16 10:57:18 +0530

    day 15

--- june2020/solutions/day15_SearchInABinarySearchTree.py
+++ june2020/solutions/day15_SearchInABinarySearchTree.py
@@ -32,3 +32,21 @@ def searchBST(root, val):
         else:
             root = root.right
     return root
+
+# Alternate Solution:
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# def searchBST(root, val):
+#     if root is None:
+#         return None
+#     if root.val == val:
+#         return root
+#     elif val < root.val:
+#         return searchBST(root.left, val)
+#     else:
+#         return searchBST(root.right, val)
+
