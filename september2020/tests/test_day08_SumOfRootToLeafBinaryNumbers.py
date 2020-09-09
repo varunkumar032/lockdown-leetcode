4f5c595f0d893c04c8457d7e974c08d27bf546d0
Varun Kumar G <varunkumar032@gmail.com>
2020-09-09 12:24:43 +0530

    day 8

--- september2020/tests/test_day08_SumOfRootToLeafBinaryNumbers.py
+++ september2020/tests/test_day08_SumOfRootToLeafBinaryNumbers.py
@@ -10,3 +10,19 @@ def test_day08_01():
     test_input_root.right.right = TreeNode(1)
     test_output = 22
     assert sumRootToLeaf(test_input_root) == test_output
+
+
+def test_day08_02():
+    test_input_root = TreeNode(1)
+    test_input_root.left = TreeNode(0)
+    test_input_root.right = TreeNode(1)
+    test_output = 5
+    assert sumRootToLeaf(test_input_root) == test_output
+
+def test_day08_03():
+    test_input_root = TreeNode(1)
+    test_input_root.left = TreeNode(0)
+    test_input_root.right = TreeNode(1)
+    test_input_root.right.right = TreeNode(1)
+    test_output = 9
+    assert sumRootToLeaf(test_input_root) == test_output
