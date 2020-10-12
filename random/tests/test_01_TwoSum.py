93307e07db8216b072a9f69f0bb90a69b69e35aa
Varun Kumar G <varunkumar032@gmail.com>
2020-10-13 02:44:10 +0530

    renamed tests 01 & 02

---
+++ random/tests/test_01_TwoSum.py
@@ -0,0 +1,25 @@
+from solutions.q_01_TwoSum import twoSum
+
+def test_01():
+    test_nums = [2,7,11,15]
+    test_target = 9
+    test_output = [0,1]
+    assert twoSum(test_nums, test_target) == test_output
+
+def test_02():
+    test_nums = [3,2,4]
+    test_target = 6
+    test_output = [1,2]
+    assert twoSum(test_nums, test_target) == test_output
+
+def test_03():
+    test_nums = [3,3]
+    test_target = 6
+    test_output = [0,1]
+    assert twoSum(test_nums, test_target) == test_output
+
+def test_04():
+    test_nums = [2,7,11,15]
+    test_target = 10
+    test_output = [-1,-1]
+    assert twoSum(test_nums, test_target) == test_output
