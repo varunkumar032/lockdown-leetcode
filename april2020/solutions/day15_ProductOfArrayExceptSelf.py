b5f15a7e068147ce495a661b5e8931883df1d95d
Varun Kumar G <varunkumar032@gmail.com>
2021-06-14 22:35:49 +0530

    alternate solution

--- april2020/solutions/day15_ProductOfArrayExceptSelf.py
+++ april2020/solutions/day15_ProductOfArrayExceptSelf.py
@@ -6,12 +6,26 @@
 
 # Note: Please solve it without division and in O(n).
 
+# def productExceptSelf(nums):
+#     result  = [1] * len(nums)
+#     for i in range(1, len(nums)):
+#         result[i] = nums[i-1] * result[i-1]
+#     R = 1
+#     for i in range(len(nums)-1, -1, -1):
+#         result[i] = result[i] * R
+#         R = R * nums[i]
+#     return result
+
+# Alternate Solution:
 def productExceptSelf(nums):
-    result  = [1] * len(nums)
-    for i in range(1, len(nums)):
-        result[i] = nums[i-1] * result[i-1]
-    R = 1
-    for i in range(len(nums)-1, -1, -1):
-        result[i] = result[i] * R
-        R = R * nums[i]
-    return result
+    n = len(nums)
+    suffixProdArray = [nums[-1]] * n
+    for i in range(n-2, -1, -1):
+        suffixProdArray[i] = nums[i] * suffixProdArray[i+1]
+    prodTillPrev = nums[0]
+    suffixProdArray[0] = suffixProdArray[1]
+    for i in range(1, n-1):
+        suffixProdArray[i] = prodTillPrev * suffixProdArray[i+1]
+        prodTillPrev *= nums[i]
+    suffixProdArray[-1] = prodTillPrev
+    return suffixProdArray
